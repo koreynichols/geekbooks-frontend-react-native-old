0e1ee1427403720ca0f6a7252857611ea6711b3e
geekwise-korey-nichols <geekwise.korey.nichols@gmail.com>
2020-03-18 08:53:44 -0700

    added input handlers for form, reset button to reset form and submit button to console.log form info

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,72 +1,141 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 const RegisterScreen = () => {
+    const[firstName, setFirstName] = useState('');
+    const[lastName, setLastName] = useState('');
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+    const[confirmPassword, setConfirmPassword] = useState('');
+
+    const resetFormHandler = () => {
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+    };
+
+    const firstNameInputHandler = inputText => {
+        setFirstName(inputText);
+    };
+
+    const lastNameInputHandler = inputText => {
+        setLastName(inputText);
+    };
+
+    const emailInputHandler = inputText => {
+        setEmail(inputText);
+    };
+
+    const passwordInputHandler = inputText => {
+        setPassword(inputText);
+    };
+
+    const confirmPasswordInputHandler = inputText => {
+        setConfirmPassword(inputText);
+    };
+
+    const submitFormHandler = () => {
+        let user = {
+                "firstName": firstName,
+                "lastName": lastName,
+                "email": email,
+                "password": password
+        };
+        console.log(user);
+        resetFormHandler();
+    }
+    
+
     return (
-        <View>
+        <View style={styles.container}>
             <View>
-                <Text>Register Form</Text>
+                <Text style={styles.title}>Register Form</Text>
             </View>
             <View style={styles.formComponent}>
-                <Text>First Name: </Text>
+                <Text style={styles.label}>First Name: </Text>
                 <TextInput 
                     style={styles.input}
                     blurOnSubmit
                     autoCapitalize='none'
                     autoCorrect={false}
+                    onChangeText={firstNameInputHandler}
+                    value={firstName}
                 />
             </View>
             <View style={styles.formComponent}>
-                <Text>Last Name: </Text>
+                <Text style={styles.label}>Last Name: </Text>
                 <TextInput
                     style={styles.input}
                     blurOnSubmit
                     autoCapitalize='none'
                     autoCorrect={false}
+                    onChangeText={lastNameInputHandler}
+                    value={lastName}
                 />
             </View>
             <View style={styles.formComponent}>
-                <Text>Email: </Text>
+                <Text style={styles.label}>Email: </Text>
                 <TextInput
                     style={styles.input}
                     blurOnSubmit
                     autoCapitalize='none'
                     autoCorrect={false}
+                    onChangeText={emailInputHandler}
+                    value={email}
                 />
             </View>
             <View style={styles.formComponent}>
-                <Text>Password: </Text>
+                <Text style={styles.label}>Password: </Text>
                 <TextInput
                     style={styles.input}
                     blurOnSubmit
                     autoCapitalize='none'
                     autoCorrect={false}
+                    onChangeText={passwordInputHandler}
+                    value={password}
                 />
             </View>
             <View style={styles.formComponent}>
-                <Text>Confirm Password: </Text>
+                <Text style={styles.label}>Confirm Password: </Text>
                 <TextInput
                     style={styles.input}
                     blurOnSubmit
                     autoCapitalize='none'
                     autoCorrect={false}
+                    onChangeText={confirmPasswordInputHandler}
+                    value={confirmPassword}
                 />
             </View>
+            <Button title='Submit' onPress={submitFormHandler} />
+            <Button title='Reset' onPress={resetFormHandler} />
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     input: {
-        width: 50,
+        width: 200,
         textAlign: 'center',
-        height: 30,
+        height: 20,
         borderColor: 'grey',
-        borderWidth: 1,
+        borderBottomWidth: 1,
         marginVertical: 10
     },
     formComponent: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        marginVertical: 20
+    },
+    label: {
+        fontSize: 20,
+        textAlignVertical: 'center'
+    },
+    container: {
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 40
     }
 });
 
