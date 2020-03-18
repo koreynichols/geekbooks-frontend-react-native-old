47db4dc3556afc616f9b401b3b44d53db6bf1041
geekwise-korey-nichols <geekwise.korey.nichols@gmail.com>
2020-03-18 09:01:13 -0700

    added login form

---
+++ screens/LoginScreen.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Text, TextInput, Button, View, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+
+
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.title}>Login Form</Text>
+            </View>
+            <View style={styles.formComponent}>
+                <Text style={styles.label}>Email: </Text>
+                <TextInput
+                    style={styles.input}
+                    blurOnSubmit
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    onChangeText={emailInputHandler}
+                    value={email}
+                />
+            </View>
+            <View style={styles.formComponent}>
+                <Text style={styles.label}>Password: </Text>
+                <TextInput
+                    style={styles.input}
+                    blurOnSubmit
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    onChangeText={passwordInputHandler}
+                    value={password}
+                />
+            </View>
+            <Button title='Submit' onPress={submitFormHandler} />
+            <Button title='Reset' onPress={resetFormHandler} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        width: 200,
+        textAlign: 'center',
+        height: 20,
+        borderColor: 'grey',
+        borderBottomWidth: 1,
+        marginVertical: 10
+    },
+    formComponent: {
+        flexDirection: 'row',
+        marginVertical: 20
+    },
+    label: {
+        fontSize: 20,
+        textAlignVertical: 'center'
+    },
+    container: {
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 40
+    }
+});
+
+export default LoginScreen;
