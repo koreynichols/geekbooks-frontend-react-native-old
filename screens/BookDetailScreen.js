6e2d6f8188104fe44e97114cfe81a921afb7f996
geekwise-korey-nichols <geekwise.korey.nichols@gmail.com>
2020-05-02 17:25:34 -0700

    working on displaying price if there is one

--- screens/BookDetailScreen.js
+++ screens/BookDetailScreen.js
@@ -9,16 +9,22 @@ const BookDetailScreen = props => {
     console.log(selectedBook.volumeInfo.title);
     return (
         <View style={styles.container}>
+            <Image style={{width: 250, height: 400}} source={{uri: selectedBook.volumeInfo.imageLinks.thumbnail}} />
             <Text style={styles.textStyles}>Title: {selectedBook.volumeInfo.title}</Text>
             <Text style={styles.textStyles}>Author: {selectedBook.volumeInfo.authors}</Text>
-            <Image style={{width: 200, height: 400}} source={{uri: selectedBook.volumeInfo.imageLinks.thumbnail}} />
+            {
+                if(selectedBook.saleInfo.saleability == "FOR_SALE"
+                    <Text style={styles.textStyles}>Price: ${selectedBook.saleInfo.retailPrice.amount}</Text> 
+                else
+                    <Text>NOT FOR SALE</Text>}
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 30
+        marginTop: 30,
+        alignItems: "center"
     },
     textStyles: {
         fontSize: 25
