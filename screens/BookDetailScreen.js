c543e2c86c96f4a4ce2297c71c4b1e0b622b455e
Korey Nichols <koreynichols1@gmail.com>
2020-11-03 13:39:42 -0800

    added: price showing if available for sale

--- screens/BookDetailScreen.js
+++ screens/BookDetailScreen.js
@@ -12,11 +12,12 @@ const BookDetailScreen = props => {
             <Image style={{width: 250, height: 400}} source={{uri: selectedBook.volumeInfo.imageLinks.thumbnail}} />
             <Text style={styles.textStyles}>Title: {selectedBook.volumeInfo.title}</Text>
             <Text style={styles.textStyles}>Author: {selectedBook.volumeInfo.authors}</Text>
-            {
-                if(selectedBook.saleInfo.saleability == "FOR_SALE"
-                    <Text style={styles.textStyles}>Price: ${selectedBook.saleInfo.retailPrice.amount}</Text> 
+            {(() => {
+                if(selectedBook.saleInfo.saleability == "FOR_SALE")
+                    return <Text style={styles.textStyles}>Price: ${selectedBook.saleInfo.retailPrice.amount}</Text> 
                 else
-                    <Text>NOT FOR SALE</Text>}
+                    return <Text>NOT FOR SALE</Text>
+                })()}
         </View>
     )
 };
