0f054a64d70445171107fbc6cc135926471a6f66
Korey Nichols <koreynichols1@gmail.com>
2020-11-04 11:23:04 -0800

    took if statement out of BookDetailScreen return and made function isForSale for price

--- screens/BookDetailScreen.js
+++ screens/BookDetailScreen.js
@@ -7,6 +7,14 @@ const BookDetailScreen = props => {
     selectedBookId = props.navigation.getParam('bookId')
     const selectedBook = bookData.items.find(book => book.id === selectedBookId);
     console.log(selectedBook.volumeInfo.title);
+
+    function isForSale(){
+        if(selectedBook.saleInfo.saleability == "FOR_SALE")
+            return <Text style={styles.textStyles}>Price: ${selectedBook.saleInfo.retailPrice.amount}</Text> 
+        else
+            return <Text style={styles.notForSale}>NOT FOR SALE</Text>
+    }
+
     return (
         <ScrollView style={styles.viewStyle}>
         <View style={styles.container}>
@@ -14,12 +22,7 @@ const BookDetailScreen = props => {
             <Text style={styles.textStyles}>Title: {selectedBook.volumeInfo.title}</Text>
             <Text style={styles.textStyles}>Author: {selectedBook.volumeInfo.authors}</Text>
             <Text style={styles.descriptionStyle}>Description: {selectedBook.volumeInfo.description}</Text>
-            {(() => {
-                if(selectedBook.saleInfo.saleability == "FOR_SALE")
-                    return <Text style={styles.textStyles}>Price: ${selectedBook.saleInfo.retailPrice.amount}</Text> 
-                else
-                    return <Text style={styles.notForSale}>NOT FOR SALE</Text>
-                })()}
+            {isForSale()}
         </View>
         </ScrollView>
     )
