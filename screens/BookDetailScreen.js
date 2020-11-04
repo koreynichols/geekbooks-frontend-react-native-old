294ba38a8f76b9e7f1428c5d05ec100c7a8ff403
Korey Nichols <koreynichols1@gmail.com>
2020-11-03 22:57:22 -0800

    playing with styling on BookDetailScreen...might be a good idea to make a new branch before doing that next time

--- screens/BookDetailScreen.js
+++ screens/BookDetailScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, ScrollView} from 'react-native';
 
 const bookData = require('../data/books-data.json');
 
@@ -8,10 +8,12 @@ const BookDetailScreen = props => {
     const selectedBook = bookData.items.find(book => book.id === selectedBookId);
     console.log(selectedBook.volumeInfo.title);
     return (
+        <ScrollView style={styles.viewStyle}>
         <View style={styles.container}>
             <Image style={{width: 250, height: 400}} source={{uri: selectedBook.volumeInfo.imageLinks.thumbnail}} />
             <Text style={styles.textStyles}>Title: {selectedBook.volumeInfo.title}</Text>
             <Text style={styles.textStyles}>Author: {selectedBook.volumeInfo.authors}</Text>
+            <Text style={styles.descriptionStyle}>Description: {selectedBook.volumeInfo.description}</Text>
             {(() => {
                 if(selectedBook.saleInfo.saleability == "FOR_SALE")
                     return <Text style={styles.textStyles}>Price: ${selectedBook.saleInfo.retailPrice.amount}</Text> 
@@ -19,20 +21,33 @@ const BookDetailScreen = props => {
                     return <Text style={styles.notForSale}>NOT FOR SALE</Text>
                 })()}
         </View>
+        </ScrollView>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
         marginTop: 30,
-        alignItems: "center"
+        marginBottom: 30,
+        alignItems: "center",
     },
     textStyles: {
-        fontSize: 25
+        fontSize: 25,
+        marginTop: 10
     },
     notForSale: {
         fontSize: 25,
+        marginTop: 10,
         color: "red"
+    },
+    descriptionStyle: {
+        fontSize: 15,
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10
+    },
+    viewStyle: {
+        backgroundColor: "#9bc8cc"
     }
 });
 
