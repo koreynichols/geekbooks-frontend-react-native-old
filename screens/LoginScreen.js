a5708a770ec20f2214a6bef17943334a963b8d00
Korey Nichols <koreynichols1@gmail.com>
2020-11-05 16:13:41 -0800

    working on styling for login and register screens

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Text, TextInput, Button, View, StyleSheet } from 'react-native';
+import AppButton from '../components/AppButton';
 
 const LoginScreen = () => {
 
@@ -30,9 +31,6 @@ const LoginScreen = () => {
 
     return (
         <View style={styles.container}>
-            <View>
-                <Text style={styles.title}>Login Form</Text>
-            </View>
             <View style={styles.formComponent}>
                 <Text style={styles.label}>Email: </Text>
                 <TextInput
@@ -55,20 +53,31 @@ const LoginScreen = () => {
                     value={password}
                 />
             </View>
-            <Button title='Submit' onPress={submitFormHandler} />
-            <Button title='Reset' onPress={resetFormHandler} />
+            <AppButton title='Submit' style={styles.submit} onPress={submitFormHandler} />
+            <AppButton title='Reset' style={styles.reset} onPress={resetFormHandler} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    submit: {
+        marginTop: 20,
+        backgroundColor: '#009688',
+    },
+    reset: {
+        marginTop: 15,
+        backgroundColor: '#A60011'
+    },
     input: {
-        width: 200,
-        textAlign: 'center',
-        height: 20,
-        borderColor: 'grey',
-        borderBottomWidth: 1,
-        marginVertical: 10
+        width: 240,
+        // textAlign: 'center',
+        height: 50,
+        backgroundColor: 'white',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 20,
+        fontSize: 18,
+        paddingHorizontal: 10
     },
     formComponent: {
         flexDirection: 'row',
@@ -79,11 +88,9 @@ const styles = StyleSheet.create({
         textAlignVertical: 'center'
     },
     container: {
-        alignItems: 'center'
+        alignItems: 'center',
+        marginTop: 100
     },
-    title: {
-        fontSize: 40
-    }
 });
 
 export default LoginScreen;
