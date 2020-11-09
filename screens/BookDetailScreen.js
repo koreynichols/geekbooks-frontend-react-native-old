572bf3f13a6d704b360ca747a9a9201520d0a026
Korey Nichols <koreynichols1@gmail.com>
2020-11-09 14:37:51 -0800

    got button working, now would like to change button to 2 tabs and change info showed based on which tab is selected

--- screens/BookDetailScreen.js
+++ screens/BookDetailScreen.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-native';
-import { Text, View, StyleSheet, Image, ScrollView} from 'react-native';
+import { Text, View, StyleSheet, Image, ScrollView, TouchableWithoutFeedback} from 'react-native';
 
 const bookData = require('../data/books-data.json');
 
 const BookDetailScreen = props => {
+
+    const [isReview, setIsReview] = useState(false);
+
     selectedBookId = props.navigation.getParam('bookId')
     const selectedBook = bookData.items.find(book => book.id === selectedBookId);
 
     console.log(selectedBook.volumeInfo.title);
-
 
     function isForSale(){
         if(selectedBook.saleInfo.saleability == "FOR_SALE")
@@ -18,36 +20,40 @@ const BookDetailScreen = props => {
             return <Text style={styles.notForSale}>NOT FOR SALE</Text>
     }
 
-    let content;
-    aboutOrReview(false);
-
-
-    function aboutOrReview(isItTrue) {
-        if(isItTrue) {
-            content = <Text>you win</Text>
-                // <View style={styles.container}>
-                //     <Text style={styles.textStyles}>Title: {selectedBook.volumeInfo.title}</Text>
-                //     <Text style={styles.textStyles}>Author: {selectedBook.volumeInfo.authors}</Text>
-                //     <Text numberOfLines={2} style={styles.descriptionStyle}>
-                //         Description: {selectedBook.volumeInfo.description}
-                //     </Text>
-                //     <Text>Read more...</Text>
-                //     {isForSale()}
-                //     <Text style={styles.textStyles}>Rating: {selectedBook.volumeInfo.averageRating} / 5</Text>
-                // </View>
-        }
-        else
-            content = <Text>Hello World</Text>
+    const confirmReviewHandler = () => {
+        console.log('hello world')
+        setIsReview(true);
     }
 
+    let content;
+
+    if(isReview) {
+        content = (
+            <View style={styles.container}>
+                <Text style={styles.textStyles}>Title: {selectedBook.volumeInfo.title}</Text>
+                <Text style={styles.textStyles}>Author: {selectedBook.volumeInfo.authors}</Text>
+                <Text numberOfLines={2} style={styles.descriptionStyle}>
+                    Description: {selectedBook.volumeInfo.description}
+                </Text>
+                <Text>Read more...</Text>
+                {isForSale()}
+                <Text style={styles.textStyles}>Rating: {selectedBook.volumeInfo.averageRating} / 5</Text>
+            </View>)
+    }
+    // if(!isReview){
+    //     content = (<Text>Hello World</Text>)
+    // }
+
     return (
-        <ScrollView style={styles.viewStyle}>
-        <View style={styles.container}>
-            <Image style={{width: 250, height: 400}} source={{uri: selectedBook.volumeInfo.imageLinks.thumbnail}} />
-        </View>
-        <Button title='hello' onPress={() => aboutOrReview(true)}></Button>
-            {content}
-        </ScrollView>
+        <TouchableWithoutFeedback>
+            <ScrollView style={styles.viewStyle}>
+            <View style={styles.container}>
+                <Image style={{width: 250, height: 400}} source={{uri: selectedBook.volumeInfo.imageLinks.thumbnail}} />
+            </View>
+            <Button title='hello' onPress={confirmReviewHandler}></Button>
+                {content}
+            </ScrollView>
+        </TouchableWithoutFeedback>
     )
 };
 
