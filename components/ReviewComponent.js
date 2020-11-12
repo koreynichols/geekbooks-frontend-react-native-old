cccfbc426accfbae8292defa57e6f1081fd3b698
Korey Nichols <koreynichols1@gmail.com>
2020-11-11 22:51:28 -0800

    starting to work on review component

---
+++ components/ReviewComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import {Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+const ReviewComponent = props => {
+    return (
+        <View>
+            <Text>Rating</Text>
+        </View>
+    );
+}
+
+
+export default ReviewComponent;
